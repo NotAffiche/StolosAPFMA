16accfec3b4cc685d095ec7484cc2476cef33448
Adrian B <adrian.biedny@student.hogent.be>
2023-05-03 22:10:56 +0200

    Certificate

--- app/src/main/java/me/adbi/stolosapfma/ActivityDetailDriver.kt
+++ app/src/main/java/me/adbi/stolosapfma/ActivityDetailDriver.kt
@@ -17,17 +17,25 @@ import me.adbi.stolosapfma.models.DriverModel
 import okhttp3.Interceptor
 import okhttp3.OkHttp
 import okhttp3.OkHttpClient
+import okhttp3.Request
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import java.io.File
+import java.io.FileInputStream
+import java.io.InputStream
+import java.security.KeyStore
 import java.security.SecureRandom
+import java.security.cert.Certificate
+import java.security.cert.CertificateFactory
 import java.security.cert.X509Certificate
 import java.util.logging.Logger
 import javax.net.ssl.SSLContext
 import javax.net.ssl.TrustManager
+import javax.net.ssl.TrustManagerFactory
 import javax.net.ssl.X509TrustManager
 
 
@@ -45,7 +53,36 @@ class ActivityDetailDriver : ComponentActivity() {
             }
         })
 
+        //region ACCEPT_SPECIFIC_TRUSTED_CERTIFICATE
+        fun readCertificateFromFile(filePath: InputStream): Certificate {
+            //val file = File(filePath)
+            //val inputStream = FileInputStream(file)
+            val certificateFactory = CertificateFactory.getInstance("X.509")
+            val certificate = certificateFactory.generateCertificate(filePath)
+            filePath.close()
+            return certificate
+        }
+
+        val keyStore = KeyStore.getInstance(KeyStore.getDefaultType())
+        keyStore.load(null, null)
+        val certificate = readCertificateFromFile(assets.open("localhost.pem"))
+        keyStore.setCertificateEntry("server_cert", certificate)
+
+        val trustManagerFactory = TrustManagerFactory.getInstance(TrustManagerFactory.getDefaultAlgorithm())
+        trustManagerFactory.init(keyStore)
+
+        val trustManagers = trustManagerFactory.trustManagers
+        val sslContext = SSLContext.getInstance("TLS")
+        sslContext.init(null, trustManagers, null)
+
+        val okHttpClient = OkHttpClient.Builder()
+            .sslSocketFactory(sslContext.socketFactory, trustManagers[0] as X509TrustManager)
+            .hostnameVerifier{_,_ -> true}
+            .build()
+        //endregion
+
         //region IGNORE_UNTRUSTED_HTTPS
+        /*
         val trustAllCerts = arrayOf<TrustManager>(object : X509TrustManager {
             override fun checkClientTrusted(chain: Array<out X509Certificate>?, authType: String?) {}
             override fun checkServerTrusted(chain: Array<out X509Certificate>?, authType: String?) {}
@@ -58,11 +95,12 @@ class ActivityDetailDriver : ComponentActivity() {
             .hostnameVerifier { _, _ -> true }
             .addInterceptor(aLogger)
             .build()
+        */
         //endregion
 
         val retrofit: Retrofit = Retrofit.Builder()
             .baseUrl(BASE_URL)
-            .client(okHttpClient)//okHttpClient defined in IGNORE_UNTRUSTED_HTTPS
+            .client(okHttpClient)//okHttpClient defined in ACCEPT_SPECIFIC_TRUSTED_CERTIFICATE//.client(okHttpClient)//okHttpClient defined in IGNORE_UNTRUSTED_HTTPS
             .addConverterFactory(GsonConverterFactory.create())
             .build()
         val api: ApiService = retrofit.create(ApiService::class.java)
@@ -186,6 +224,7 @@ class ActivityDetailDriver : ComponentActivity() {
         } else {// code for creating new driver
             btnDelete.setVisibility(View.GONE)
             //region REGISTER ADD
+
             btnSave.setOnClickListener(View.OnClickListener {
                 var licenses = ArrayList<String>()
                 for (l in evLicenses.text.toString().split(",")) {
