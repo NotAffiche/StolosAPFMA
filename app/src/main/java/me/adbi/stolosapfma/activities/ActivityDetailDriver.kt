762ad03e8ffbc7842be3bc0ec1eba6e8a38898ec
Adrian B <adrian.biedny@student.hogent.be>
2023-06-09 00:51:40 +0200

    multi select drivers licenses

--- app/src/main/java/me/adbi/stolosapfma/activities/ActivityDetailDriver.kt
+++ app/src/main/java/me/adbi/stolosapfma/activities/ActivityDetailDriver.kt
@@ -1,5 +1,7 @@
 package me.adbi.stolosapfma
 
+import android.app.AlertDialog
+import android.content.DialogInterface
 import android.content.Intent
 import android.os.Bundle
 import android.text.Editable
@@ -20,6 +22,8 @@ import retrofit2.Response
 
 
 class ActivityDetailDriver : ComponentActivity() {
+
+    var licensesToSave: String = ""
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -57,7 +61,7 @@ class ActivityDetailDriver : ComponentActivity() {
         val evLastName = findViewById<EditText>(R.id.evLastName)
         val evBirthDate = findViewById<EditText>(R.id.evBirthDate)
         val evRRN = findViewById<EditText>(R.id.evRRN)
-        val evLicenses = findViewById<EditText>(R.id.evLicenses)
+        val tvLicensesSelect = findViewById<TextView>(R.id.tvLicensesSelect)
         val evAddress = findViewById<EditText>(R.id.evAddress)
         val evVehicle = findViewById<EditText>(R.id.evVehicle)
         val evGasCard = findViewById<EditText>(R.id.evGasCard)
@@ -66,6 +70,48 @@ class ActivityDetailDriver : ComponentActivity() {
         //region BUTTONS
         val btnSave: Button = findViewById(R.id.btnSave)
         val btnDelete = findViewById<Button>(R.id.btnDelete)
+        //endregion
+
+        val licenses = resources.getStringArray(R.array.licenses)
+        var selectedLicenses = BooleanArray(licenses.size) { false }
+        val selection = mutableListOf<Int>()
+        //region licenses select
+        tvLicensesSelect.setOnClickListener {
+
+            val licenseSelectionClickListener = DialogInterface.OnMultiChoiceClickListener { _, which, isChecked ->
+                if (isChecked) {
+                    selection.add(which)
+                } else {
+                    selection.remove(which)
+                }
+            }
+
+            val positiveButtonClickListener = DialogInterface.OnClickListener { dialog, _ ->
+                val strb = StringBuilder()
+                for (i in 0 until selection.size) {
+                    strb.append(licenses[selection[i]])
+                    if (i != selection.size - 1) {
+                        strb.append(",")
+                    }
+                }
+                tvLicensesSelect.text = strb.toString()
+                licensesToSave = strb.toString()
+                Log.i("ADBILOGSTOLOS", licensesToSave)
+                dialog.dismiss()
+            }
+
+            val negativeButtonClickListener = DialogInterface.OnClickListener { dialog, _ ->
+                dialog.dismiss()
+            }
+
+            val alertBuilder = AlertDialog.Builder(this)
+            alertBuilder.setMultiChoiceItems(licenses, selectedLicenses, licenseSelectionClickListener)
+                .setPositiveButton("OK", positiveButtonClickListener)
+                .setNegativeButton("Cancel", negativeButtonClickListener)
+
+            val alertDialog = alertBuilder.create()
+            alertDialog.show()
+        }
         //endregion
 
         val driverId: Int = intent.getIntExtra("driverID", 0)
@@ -82,7 +128,24 @@ class ActivityDetailDriver : ComponentActivity() {
                         evLastName.text = Editable.Factory.getInstance().newEditable(d.lastName)
                         evBirthDate.text = Editable.Factory.getInstance().newEditable(d.birthDate.split("T")[0])
                         evRRN.text = Editable.Factory.getInstance().newEditable(d.natRegNum)
-                        evLicenses.text = Editable.Factory.getInstance().newEditable(d.licenses.joinToString(separator = ","))
+                        //
+                        for (license in d.licenses) {
+                            val index = licenses.indexOf(license)
+                            selection.add(index)
+                            if (index != -1) {
+                                selectedLicenses[index] = true
+                            }
+                        }
+                        val strb = StringBuilder()
+                        for (i in 0 until d.licenses.size) {
+                            strb.append(d.licenses[i])
+                            if (i != d.licenses.size - 1) {
+                                strb.append(",")
+                            }
+                        }
+                        tvLicensesSelect.text = strb.toString()
+                        licensesToSave = strb.toString()
+                        //
                         evAddress.text = Editable.Factory.getInstance().newEditable("")
                         evVehicle.text = Editable.Factory.getInstance().newEditable("")
                         evAddress.text = Editable.Factory.getInstance().newEditable("")
@@ -100,7 +163,7 @@ class ActivityDetailDriver : ComponentActivity() {
                         //region REGISTER UPDATE
                         btnSave.setOnClickListener(View.OnClickListener {
                             var updatedD = DriverModel(driverId, evFirstName.text.toString(), evLastName.text.toString(), evBirthDate.text.toString(),
-                            evRRN.text.toString(), evLicenses.text.toString().split(","), evAddress.text.toString(), evVehicle.text.toString(), evGasCard.text.toString())
+                            evRRN.text.toString(), licensesToSave.split(","), evAddress.text.toString(), evVehicle.text.toString(), evGasCard.text.toString())
                             val call: Call<Unit> = api.updateDriverById(updatedD)
                             call.enqueue(object : Callback<Unit> {
                                 override fun onResponse(call: Call<Unit>, response: Response<Unit>) {
@@ -148,17 +211,13 @@ class ActivityDetailDriver : ComponentActivity() {
             //region REGISTER ADD
 
             btnSave.setOnClickListener{
-                var licenses = ArrayList<String>()
-                for (l in evLicenses.text.toString().split(",")) {
-                    licenses.add("$l")
-                }
                 var cDriver = DriverModel(
                 null,
                     evFirstName.text.toString(),
                     evLastName.text.toString(),
                     evBirthDate.text.toString(),
                     evRRN.text.toString(),
-                    licenses,
+                    licensesToSave.split(","),
                     evAddress.text.toString(),
                     evVehicle.text.toString(),
                     evGasCard.text.toString()
